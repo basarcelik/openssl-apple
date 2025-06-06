87741869064b40b3fd7ae1b599e5f503b9e558b8
Basar Celik <33145552+basarcelik@users.noreply.github.com>
2025-06-06 12:28:42 +0300

    Package Swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let filename = "openssl.xcframework.zip"
-let version = "3.4.200"
+let version = "3.4.1.s"
 let checksum = "f6810dbc37a42e0bc7ceca44716987a3f40629d3c79967a5626e12b0bb93b65a"
 
 let package = Package(
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/passepartoutvpn/openssl-apple/releases/download/\(version)/\(filename)",
+            url: "https://github.com/basarcelik/openssl-apple/releases/download/\(version)/\(filename)",
             checksum: checksum
         ),
 
